1c6774908f9f55e91e0324caf0592afaf22a1ae6
jonathanstevanka143 <jonathanstevanka@yahoo.com>
2019-02-26 12:37:55 -0500

    hotfix
    -fixed bug that would prevent search from happening or remaining 'null'

--- app/src/main/java/com/example/gamershub/MainActivity.java
+++ app/src/main/java/com/example/gamershub/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.gamershub;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -15,24 +18,28 @@ import com.example.gamershub.igdbAPI.APICOMMAND;
 
 import org.json.JSONArray;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HomeScreen.OnFragmentInteractionListener{
 
     private TextView mTextMessage;
 
+    FragmentManager fm;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentTransaction transaction = fm.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    transaction.replace(R.id.content, new HomeScreen());
+                    transaction.addToBackStack(null);
+                    transaction.commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -43,19 +50,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        fm = getSupportFragmentManager();
         //create a new APIcommand class so we can acess our custom methods throughout the application
         APICOMMAND apicommand = new APICOMMAND();
         //create a new androidnetworking class so we can have access to the networking class to carry out the calls
-        AndroidNetworking androidNetworking= null;
+
+        //initialize
+        AndroidNetworking.initialize(getApplicationContext());
+
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        androidNetworking.initialize(getApplicationContext());
         //call the search command
-        apicommand.Search(androidNetworking,"Call of Duty");
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
--- app/src/main/java/com/example/gamershub/igdbAPI/APICOMMAND.java
+++ app/src/main/java/com/example/gamershub/igdbAPI/APICOMMAND.java
@@ -31,24 +31,24 @@ public class APICOMMAND {
         });
     }
 
-    public ArrayList<JSONObject> Search(AndroidNetworking androidNetworking,String search){
+    public ArrayList<JSONObject> Search(String search){
         //create an arraylist of jsonobjects so we an splice the response we get back later on from JSON
         final ArrayList<JSONObject> contents = new ArrayList<JSONObject>();
 
-        AndroidNetworking.post("https://api-v3.igdb.com/games/").addHeaders("user-key", BuildConfig.IGDBKey)
-                .addHeaders("Accept","application/json").addStringBody("search \""+search+"\"; fields name,popularity,rating;")
-                .setPriority(Priority.MEDIUM).build().getAsJSONArray(new JSONArrayRequestListener() {
+        AndroidNetworking.post("https://api-v3.igdb.com/games/").addHeaders("user-key",BuildConfig.IGDBKey)
+                .addHeaders("Accept","application/json").addHeaders("Content-Type","application/x-www-form-urlencoded").addStringBody("search \""+search+"\"; fields name,popularity,rating;")
+                .setPriority(Priority.HIGH).build().getAsJSONArray(new JSONArrayRequestListener() {
             @Override
             public void onResponse(JSONArray response) {
                 //use the string search method to retreive whatever the user was searching for.
                 //this allows for easy modification, and we seperate it into arraylists for easy implementation into card views
-                //for(int i=0; i < response.length(); i++){
+                for(int i=0; i < response.length(); i++){
                     //add the contents to our local arraylist
-                    //contents.add(response.optJSONObject(i));
-                    //TESTING-print to the console
-                    //System.out.println(contents.get(i));
-                //}
-                System.out.println(response);
+                    contents.add(response.optJSONObject(i));
+                    //TESTING-print to the console1
+                    System.out.println(contents.get(i));
+                }
+
             }
 
             @Override
