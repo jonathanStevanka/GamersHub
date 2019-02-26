b2dd96d6cefa7384b37e6983e48a4305f1bfc2be
jonathanstevanka143 <jonathanstevanka@yahoo.com>
2019-02-25 20:18:39 -0500

    Initial setup
    #4 created simple methods to access the API(hidden api)

---
+++ app/src/main/java/com/example/gamershub/igdbAPI/APICOMMAND.java
@@ -0,0 +1,46 @@
+package com.example.gamershub.igdbAPI;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
+import org.json.JSONArray;
+
+public class APICOMMAND {
+
+
+    public void Search(AndroidNetworking androidNetworking){
+        AndroidNetworking.post("https://api-v3.igdb.com/games/").addHeaders("user-key","YOUR KEY HERE")
+                .addHeaders("Accept","application/json").addStringBody("fields name,popularity,rating; sort popularity desc;")
+                .setPriority(Priority.MEDIUM).build().getAsJSONArray(new JSONArrayRequestListener() {
+            @Override
+            public void onResponse(JSONArray response) {
+                System.out.println(response);
+            }
+
+            @Override
+            public void onError(ANError anError) {
+
+            }
+        });
+    }
+
+    public void Search(AndroidNetworking androidNetworking, String search){
+        AndroidNetworking.post("https://api-v3.igdb.com/games/").addHeaders("user-key","YOUR KEY HERE")
+                .addHeaders("Accept","application/json").addStringBody("fields name,popularity,rating; sort popularity desc;")
+                .setPriority(Priority.MEDIUM).build().getAsJSONArray(new JSONArrayRequestListener() {
+            @Override
+            public void onResponse(JSONArray response) {
+                System.out.println(response);
+            }
+
+            @Override
+            public void onError(ANError anError) {
+
+            }
+        });
+    }
+
+
+}
